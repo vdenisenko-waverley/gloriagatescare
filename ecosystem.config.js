d323c8cda78c85df41e63beb8c415cf1818da6b0
Vitalii Denysenko <vdenisenko@waverleysoftware..com>
2024-02-19 16:16:08 +0100

    Add deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "npm",
+      args: "run start",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    prod: {
+      key: "~/.ssh/id_rsa",
+      user: "ubuntu",
+      host: ["3.101.71.238"],
+      ref: "origin/main",
+      repo: "git@github.com:vdenisenko-waverley/gloriagatescare.git",
+      path: "/home/ubuntu/tmp-site",
+      "post-deploy":
+        "NODE_ENV=production npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
