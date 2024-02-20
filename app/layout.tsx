8c6d251d47be6c3ea617fc939e09bf768767ba63
Vitalii Denysenko <vdenisenko@waverleysoftware..com>
2024-02-20 10:52:08 +0100

    Add viewport meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
 import type { Metadata } from "next";
+import type { Viewport } from "next";
 import App from "./app";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Gloria Gates Care",
   description: "Gloria Gates Care App",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  interactiveWidget: "resizes-content",
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
